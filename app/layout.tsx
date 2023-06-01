9b1326af09465832e301675af9cd7097df7ab28a
xinyu63 <101320806+xinyu63@users.noreply.github.com>
2023-06-01 18:48:22 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "互联网格局讨论",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
